5f1ee7c62efe055fed9488b580f3d6918350c467
prabhrajput2009-cell <prabhrajput2009@gmail.com>
2026-01-26 12:59:38 +0530

    Added card flip logic

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ btn.addEventListener("click", () => {
     window.location.href = "page2.html";
   }, 800);
 });
+let flippedCount = 0;
+
+function flipCard(card) {
+  if (!card.classList.contains("flipped")) {
+    card.classList.add("flipped");
+    flippedCount++;
+
+    if (flippedCount === 3) {
+      document.getElementById("finalBtn").style.display = "inline-block";
+    }
+  }
+}
+
+function goFinal() {
+  window.location.href = "page3.html";
+}
